ef4997e6cc1a7c49ac1c728259367bac5e527c9b
Colin Hulme <17675453+ch359@users.noreply.github.com>
2019-01-09 16:16:17 +0000

    11 - Adding docking ability

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,20 @@
+require_relative 'bike'
+
 class DockingStation
+
+  attr_reader :bike
+
+  def initialize
+    @bike = nil
+  end
 
   def release_bike
     Bike.new
   end
+
+  def dock(bike)
+    @bike = bike
+  end
+
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,14 +4,26 @@ describe DockingStation do
 
   it { is_expected.to respond_to :release_bike }
 
-  #it { is_expected.to be_an_instance_of(Bike)}
-
   it "Should release a bike object" do
     expect(subject.release_bike.class).to eq Bike
   end
 
   it "It should release a working bike" do
-    expect(subject.release_bike).to respond_to(:working?)
+    expect(subject.release_bike).to be_working
   end
 
+  it { is_expected.to respond_to(:dock).with(1) }
+
+  it { is_expected.to respond_to(:bike) }
+
+  it "docks a bike" do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq(bike)
+  end
+
+  it "returns docked bikes" do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq(bike)
+  end
 end
