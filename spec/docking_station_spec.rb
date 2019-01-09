9e7a9c2529d26e86e696132af415d0947f0d9a5a
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-01-09 15:13:50 +0000

    Step 10 - Make docking station release bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,4 +4,14 @@ describe DockingStation do
 
   it { is_expected.to respond_to :release_bike }
 
+  #it { is_expected.to be_an_instance_of(Bike)}
+
+  it "Should release a bike object" do
+    expect(subject.release_bike.class).to eq Bike
+  end
+
+  it "It should release a working bike" do
+    expect(subject.release_bike).to respond_to(:working?)
+  end
+
 end
